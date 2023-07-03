38ff57935ffb87079f4c0d129486ba66404df450
Jason Park <jjpark987@gmail.com>
2023-07-03 12:50:52 -0700

    commit tree fe254cc7c62e3a06e3ac7ec51e9eb6792a743d5b

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,5 @@
 class ItemsController < ApplicationController
+    def index 
+        render json: Item.all, except: [:created_at, :updated_at], include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+    def show 
+        render json: User.find(params[:id]), except: [:created_at, :updated_at], include: [:items]
+    rescue ActiveRecord::RecordNotFound
+        render json: { error: 'User not found' }, status: :not_found
+    end
 end
